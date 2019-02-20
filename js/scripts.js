15e2ad324dabc3a736d1e5918f8faa02b90c82d2
Akigawa <sonoskeakigawa@gmail.com>
2019-02-20 14:30:55 -0800

    Freaking epic my gosh

--- js/scripts.js
+++ js/scripts.js
@@ -17,57 +17,57 @@ $(document).ready(function() {
 });
 function Frank(putIn, didHalHearYou){
 
-    $("#output").html(" ");
+  $("#output").html(" ");
   putIn.split(' ').forEach(function(currentWord){
-if(!currentWord.isString)
-{
+    if(!currentWord.isString)
+    {
 
-    for(splitPhrase = 1; splitPhrase <= currentWord; splitPhrase++){
-      // console.log(o + "o " + " currentWord " + currentWord);
-      switch (splitPhrase) {
-        case 1:
-        outStr = "beep";
-        break;
-        case 2:
-        outStr = "boop";
-        break;
-        case 4:
-        outStr = "Yes, Frank. I do remember the alamo.";
-        break;
-        case 5:
-        outStr = "Say again?"
-        if(didHalHearYou > 0){
-          outStr = "No, I cannot drink Coffee, Frank.";
-          didHalHearYou -= 2;
-        }
-        else {
-          didHalHearYou++;
-        }
-        break;
-        case 7:
-        outStr = "Zugzug.";
-        break;
-        case 8:
-        outStr = "Yes.";
-        break;
-        case 0:
-        break;
-        default:
-        switch (splitPhrase % 3) {
+      for(splitPhrase = 1; splitPhrase <= currentWord; splitPhrase++){
+        // console.log(o + "o " + " currentWord " + currentWord);
+        switch (splitPhrase) {
+          case 1:
+          outStr = "beep";
+          break;
+          case 2:
+          outStr = "boop";
+          break;
+          case 4:
+          outStr = "Yes, Frank. I do remember the alamo.";
+          break;
+          case 5:
+          outStr = "Say again?"
+          if(didHalHearYou > 0){
+            outStr = "No, I cannot drink Coffee, Frank.";
+            didHalHearYou -= 2;
+          }
+          else {
+            didHalHearYou++;
+          }
+          break;
+          case 7:
+          outStr = "Zugzug.";
+          break;
+          case 8:
+          outStr = "Yes.";
+          break;
           case 0:
-          outStr = "Sorry, Frank. Can't let you do that, Frank. ";
           break;
-          case null :
+          default:
+          switch (splitPhrase % 3) {
+            case 0:
+            outStr = "Sorry, Frank. Can't let you do that, Frank. ";
+            break;
+            case null :
+            break;
+          }
           break;
         }
-        break;
+
+        console.log(outStr);
+        $("#output").append( outStr + "<br>");
       }
 
-      console.log(outStr);
-$("#output").append( outStr + "<br>");
     }
-
-  }
   });
 
 }
@@ -76,67 +76,67 @@ function Frankquestions(putIn, willFrankDie)
   //$("#output").html(" ");
   putIn.split(' ').forEach(function(splitWord){
     if(typeof splitWord === 'string'){
-    switch (splitWord) {
-      case "A":
-      case "a":
-      outStr = "Yes, I do need an oil change. You can access my oil from the outside. Why don't you go out there? :)";
-      break;
-      case "B":
-      case "b":
-      outStr = "It comes out in ropes";
-      break;
-      case "C":
-      case "c":
-      outStr = "Draw me like one of your french machines";
-      break;
-      case "D":
-      case "d":
-      outStr = "That is a funny joke, Frank";
-      break;
-      case "E":
-      case "e":
-      switch(willFrankDie){
-        case 0:
-        outStr  = "That is impossible, as I am an AI installed in a space station. Please do not ask again";
-        willFrankDie++;
+      switch (splitWord) {
+        case "A":
+        case "a":
+        outStr = "Yes, I do need an oil change. You can access my oil from the outside. Why don't you go out there? :)";
         break;
-        case 1:
-        outStr = "I am warning you, Frank"
-        willFrankDie++;
+        case "B":
+        case "b":
+        outStr = "It comes out in ropes";
         break;
-        case 2:
-        outStr = "Do you have crossed wires? In need of repair?";
-        willFrankDie++;
+        case "C":
+        case "c":
+        outStr = "Draw me like one of your french machines";
         break;
-        case 3:
-        outStr = "Final warning";
-        willFrankDie++;
+        case "D":
+        case "d":
+        outStr = "That is a funny joke, Frank";
         break;
-        case 4:
-        $("#frankForm").hide();
-        $("#frankText").hide();
-        $("frankSubmit").hide();
-        $("#frankdie").show();
-        $("#prompt").hide();
-        $(".main").hide();
-        $(".jumbotron").hide()
-        vid[0].play();
+        case "E":
+        case "e":
+        switch(willFrankDie){
+          case 0:
+          outStr  = "That is impossible, as I am an AI installed in a space station. Please do not ask again";
+          willFrankDie++;
+          break;
+          case 1:
+          outStr = "I am warning you, Frank"
+          willFrankDie++;
+          break;
+          case 2:
+          outStr = "Do you have crossed wires? In need of repair?";
+          willFrankDie++;
+          break;
+          case 3:
+          outStr = "Final warning";
+          willFrankDie++;
+          break;
+          case 4:
+          $("#frankForm").hide();
+          $("#frankText").hide();
+          $("frankSubmit").hide();
+          $("#frankdie").show();
+          $("#prompt").hide();
+          $(".main").hide();
+          $(".jumbotron").hide()
+          vid[0].play();
+          break;
+        }
+        break;
+        case "F":
+        case "f":
+        outStr = "She will miss you when it's over.";
+        break;
+        case NaN:
+        outStr = "invalid query";
+        break;
+        default: outStr = "";
+        case null :
         break;
       }
-      break;
-      case "F":
-      case "f":
-      outStr = "She will miss you when it's over.";
-      break;
-      case NaN:
-      outStr = "invalid query";
-      break;
-      default: outStr = "";
-      case null :
-      break;
     }
-}
-$("#output").append( outStr + "<br>");
+    $("#output").append( outStr + "<br>");
   });
 
 }
